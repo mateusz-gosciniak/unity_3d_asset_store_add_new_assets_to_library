9bef40ba63885c32e8314d45bcd9bef605a09fd8
Mateusz Gryfin Gościniak <gosciniak.mateusz@outlook.com>
2024-06-27 16:01:56 +0200

    extend Chrome Driver class by added enable_logs parameter and more extendend options configuration

--- add_new_free_assets_to_library.py
+++ add_new_free_assets_to_library.py
@@ -1,26 +1,63 @@
 """ File which run automation to grab new free assets from Unity 3D Store. """
 
 import logging
+import subprocess
 
 import bs4
 import requests
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome, ChromeOptions, ChromeService
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class ChromeDriver:
-    def __init__(self) -> None:
+    def __init__(self, enable_logs=False) -> None:
+        # enable logs 
+        service = ChromeService(service_args=['--log-level=INFO'], 
+                                log_output=subprocess.STDOUT) \
+            if enable_logs else ChromeService()
+        # configure chrome engine
+        options = ChromeDriver.make_options(enable_logs)
+        # make selenium Chrome webbrowser object
+        self.chrome_driver = Chrome(options, service)
+        logging.info("Chrome init complete")
+
+    @staticmethod
+    def make_options(enable_logs=False):
         options = ChromeOptions()
+        
+        # more options:
+        # https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md
+        # https://peter.sh/experiments/chromium-command-line-switches/
         additional_arguments = [
-            "start-maximized"
+            "incognito",
+            "start-maximized",
+            "disable-popup-blocking",
+            "disable-default-apps",
+            "disable-extensions",
+            "enable-automation",
+            "no-sandbox",
+            "disable-infobars",
+            "disable-notifications",
+            "disable-save-password-bubble",
+            "disable-translate",
+            "mute-audio",
+            "hide-scrollbars",
+            "no-default-browser-check",
+            "disable-search-engine-choice-screen",
         ]
+
+        if enable_logs:
+            additional_arguments.append("log-level=0")
+            additional_arguments.append("enable-logging=stdout")
+
         for arg in additional_arguments:
             options.add_argument(arg)
-
-        self.chrome_driver = Chrome(options)
-        logging.info("Chrome init complete")
+        
+        experimental_options_prefs = {}
+        options.add_experimental_option("prefs", experimental_options_prefs)
+        return options
 
     def __enter__(self):
         return self.chrome_driver
