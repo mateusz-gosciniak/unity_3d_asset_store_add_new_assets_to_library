b4a202f59ab406754b05fcc9fef2d6185e619c0b
Mateusz Gryfin Gościniak <gosciniak.mateusz@outlook.com>
2024-06-28 12:38:06 +0200

    project settings provided

---
+++ project_settings.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ProjectSettings:
+    UNITY_CREDENTIALS_NAME = "unity_id_cred"
+    UNITY_CREDENTIALS_EMAIL = "mateusz.gosciniak.dev@gmail.com"
