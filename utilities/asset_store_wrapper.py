cd7f3b8da4b4376d0eef457af2dba4998b3cd85c
Mateusz Gryfin Gościniak <gosciniak.mateusz@outlook.com>
2024-07-01 14:17:09 +0200

    AssetStoreWrapper added additional popup behaviour

--- utilities/asset_store_wrapper.py
+++ utilities/asset_store_wrapper.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import NoSuchElementException
 
 # in project packages
 from utilities.chrome_driver import ChromeDriver
@@ -22,6 +23,10 @@ class AssetStoreWrongPageError(Exception):
 
 
 class AssetStoreAssetButtonError(Exception):
+    pass
+
+
+class AssetStorePopupNotRecognized(Exception):
     pass
 
 
@@ -128,21 +133,94 @@ class AssetStoreWrapper(ChromeDriver):
         if add_asset_btn is None:
             raise AssetStoreAssetButtonError("Button not found")
 
+        if add_asset_btn.text.strip().lower() != "Add to My Assets".lower().strip():
+            raise AssetStoreAssetButtonError("Button not found")
+        
         add_asset_btn.click()
         logging.info(f"Add asset clicked")
 
-        accept_btn_xpath = "//button[@label='Accept']"
-        accept_btn = self.wait_for_element(By.XPATH, accept_btn_xpath)
-        self.waiter.until(EC.element_to_be_clickable(accept_btn))
-        accept_btn.click()
-        logging.info(f"Accept button clicked")
+        sleep(0.5)
+
+        try:
+            accept_btn_clicked = False
+            accept_btn_xpath = "//button[@label='Accept']"
+            accept_btn = self.wait_for_element(By.XPATH, accept_btn_xpath)
+            self.waiter.until(EC.element_to_be_clickable(accept_btn))
+            accept_btn.click()
+            accept_btn_clicked = True 
+            logging.info(f"Accept button clicked")
+        except NoSuchElementException:
+            logging.info(f"Additional popup obtain, Accept button element not found")
         
+        sleep(0.5)
+        accept_and_add_btn_clicked = False
+        try:
+            popup = self.chrome.find_element(By.XPATH, "//div[@data-test='email-opt-in-dialogue']")
+            sleep(0.5)
+            self.waiter.until(EC.visibility_of(popup))
+            if "accept & add to my assets" not in popup.text.lower().strip():
+                raise AssetStorePopupNotRecognized("Popup not recognized")
+            
+            logging.info(f"Additional popup obtained")
+            
+            checkbox_1_xpath = "//div[@data-test='email-opt-in-terms-section']"
+            checkbox_1 = popup.find_element(By.XPATH, checkbox_1_xpath)
+            checkbox_1_btn = checkbox_1.find_element(By.XPATH, ".//button")
+            checkbox_1_btn.click()
+            
+            checkbox_2_xpath = "//div[@data-test='email-opt-in-privacy-policy-section']"
+            checkbox_2 = popup.find_element(By.XPATH, checkbox_2_xpath)
+            checkbox_2_btn = checkbox_2.find_element(By.XPATH, ".//button")
+            checkbox_2_btn.click()
+
+            checkbox_3_xpath = "//div[@data-test='email-opt-in-marketing-activities-section']"
+            checkbox_3 = popup.find_element(By.XPATH, checkbox_3_xpath)
+            checkbox_3_btn = checkbox_3.find_element(By.XPATH, ".//button")
+            checkbox_3_btn.click()
+            
+            accept_and_add_btn_xpath = "//button[@data-test='email-opt-in-accept-button']"
+            accept_and_add_btn = popup.find_element(By.XPATH, accept_and_add_btn_xpath)
+
+            if not accept_and_add_btn:
+                raise AssetStorePopupNotRecognized("Popup not recognized")
+            
+            logging.info(f"accept_and_add_btn text = {accept_and_add_btn.text.lower().strip()}")
+            # if "accept & add to my assets" in accept_and_add_btn.text.lower().strip():
+            #     raise AssetStorePopupNotRecognized("Popup not recognized")
+
+            accept_and_add_btn.click()
+            accept_and_add_btn_clicked = True     
+            
+        except NoSuchElementException:
+            if accept_btn_clicked:
+                logging.info(f"No additional popup ok")
+            elif accept_and_add_btn_clicked:
+                logging.info(f"popup clicked ok")
+            else:
+                raise AssetStorePopupNotRecognized("Popup not recognized")
+        
+        sleep(0.5)
+
+        if not accept_btn_clicked:
+            try:
+                accept_btn_xpath = "//button[@label='Accept']"
+                accept_btn = self.wait_for_element(By.XPATH, accept_btn_xpath)
+                self.waiter.until(EC.element_to_be_clickable(accept_btn))
+                accept_btn.click()
+                accept_btn_clicked = True
+                logging.info(f"Accept button clicked")
+            except NoSuchElementException:
+                if not accept_btn_clicked:
+                    raise AssetStorePopupNotRecognized("Popup not recognized")
+
+        sleep(0.5)
+
         # Added to My Assets Popup check
         added_popup_xpath = "//div[text()[contains(.,'Added to My Assets')]]"
         added_popup = self.wait_for_element(By.XPATH, added_popup_xpath)
         self.waiter.until(EC.element_to_be_clickable(added_popup))
         logging.info(f"Added to My Assets Popup appear")
-        
+
         # close popup by sending Escape key
         sleep(1)
         ActionChains(self.chrome).send_keys(Keys.ESCAPE).perform()
