c657be0d2328a59bf2948feb1720891dca1810f9
Mateusz Gryfin Gościniak <gosciniak.mateusz@outlook.com>
2024-07-01 14:17:54 +0200

    Add make screen if error and bugfix main logic

--- add_new_free_assets_to_library.py
+++ add_new_free_assets_to_library.py
@@ -4,6 +4,7 @@
 import logging
 import os
 import sys
+import time
 import traceback
 from pathlib import Path
 from re import compile
@@ -12,6 +13,7 @@ from datetime import datetime
 # third-party packages
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementClickInterceptedException
+from pyautogui import screenshot
 
 # in project packages
 from utilities.asset_store_wrapper import AssetStoreWrapper, AssetStoreAssetButtonError
@@ -26,6 +28,7 @@ class AutomationAddNewFreeAssetsToLib():
     def __init__(self, unity_credentials, log_folder_path) -> None:
         self.unity_credentials = unity_credentials
         self.added_assets = []
+        self.general_exception_count = 0
         self.intercepted_exception_count = 0
         self.log_folder_path = log_folder_path
 
@@ -67,9 +70,6 @@ class AutomationAddNewFreeAssetsToLib():
 
         # Iter by founded assets
         for asset in assets:
-            # Add info about assets
-            self._collect_asset_info(asset)
-
             # Check asset status
             asset_status = asset_store.get_asset_status(asset).lower().strip()
 
@@ -98,25 +98,55 @@ class AutomationAddNewFreeAssetsToLib():
                 self.intercepted_exception_count += 1 
                 logging.info(f"Something goes wrong with this asset during adding: {ex}")
                 return False
+            except Exception as ex:
+                if self.general_exception_count == 3:
+                    raise ex
+                self.general_exception_count += 1
+                logging.info(f"Something goes wrong with this asset during adding: {ex}")
+                return False
             else:
-                self.intercepted_exception_count == 0
+                # Reset exception counters
+                self.general_exception_count = 0
+                self.intercepted_exception_count = 0
+
+                # Add info about assets
+                self._collect_asset_info(asset)
 
         logging.info("Page finished - refreshing")
         return False
 
-    def run(self):
-        asset_store = None
+    def _make_screen(self, asset_store):
+        # 1st Screen Making try
+        log_screen_path = os.path.join(self.log_folder_path, "screen_log.png")
+        screen_done = False
         try:
-            with AssetStoreWrapper() as asset_store:
+            asset_store.chrome.save_screenshot(log_screen_path)
+            screen_done = True
+        except Exception as ex:
+            logging.error(f"Error during makeing screen: {ex}")
+        # 2nd Screen Making try if before fail
+        if not screen_done:
+            logging.info("Making screen second try")
+            try:
+                screenshot(log_screen_path)
+            except Exception as ex:
+                logging.error(f"Error during makeing screen 2nd time: {ex}")
+                logging.info("No screen avaiable")
+
+    def run(self):
+        with AssetStoreWrapper() as asset_store:
+            try:
                 asset_store.login_to_asset_store(self.unity_credentials)
+                page_start_time = time.time()
                 while not self._get_assets_per_page(asset_store):
                     logging.info(f"Found more assets - page refreshing")
+                    page_end_time = time.time() - page_start_time
+                    logging.info(f"time to get asset from page take: {page_end_time}s")
                 logging.info("All Assets Processed and should be added to account")
-        except Exception as ex:
-            if asset_store:
-                log_screen_path = os.path.join(self.log_folder_path, "screen_log.png")
-                asset_store.chrome.save_screenshot(log_screen_path)
-            raise ex
+            except Exception as main_ex:
+                logging.error(f"Error during processing automation: {main_ex}")
+                self._make_screen(asset_store)
+                raise main_ex
 
 
 def main():    
@@ -141,8 +171,10 @@ def main():
     unity_credentials = (ProjectSettings.UNITY_CREDENTIALS_NAME, ProjectSettings.UNITY_CREDENTIALS_EMAIL)
 
     logging.info("=== START ===")
+    
     automation = None
     try:
+        automation_start_time = time.time()
         logging.info("Automation started...")
         automation = AutomationAddNewFreeAssetsToLib(unity_credentials, log_folder_path)
         automation.run()  # <-- entry point
@@ -153,7 +185,11 @@ def main():
         logging.info("Automation finished with success")
     finally:
         if automation:
-            logging.info(f"Please check added assets: {automation.added_assets}")
+            logging.info(f"Added {len(automation.added_assets)} assets.")
+            logging.info(f"Please check details of added assets: \n{automation.added_assets}")
+            
+        automation_end_time = time.time() - automation_start_time
+        logging.info(f"Automation take: {automation_end_time}s")
         logging.info("=== END ===")
 
 
--- project_settings.py
+++ project_settings.py
@@ -2,5 +2,5 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class ProjectSettings:
-    UNITY_CREDENTIALS_NAME = "unity_id_cred"
-    UNITY_CREDENTIALS_EMAIL = "mateusz.gosciniak.dev@gmail.com"
+    UNITY_CREDENTIALS_NAME = "UNITY_ACCOUNT"
+    UNITY_CREDENTIALS_EMAIL = "gryf.tm@gmail.com"
